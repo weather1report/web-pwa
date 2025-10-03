3395664c804c685c927739c98ee996139739099e
Egor Ermakov <78217976+weather1report@users.noreply.github.com>
2025-10-04 00:04:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const weight = this.getElementById('weight')
+    const height = this.getElementById('height')
+    const output = this.getElementById('imt')
+    const button = this.getElementById('btn')
+
+
+    button.addEventListener('click', function () {
+        const w = (+weight.value)
+        const h = (+height.value) / 100
+        const imt = w / h / h
+        output.value = imt.toFixed(2)
+    })
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+                console.log('SW registred', registration)
+            })
+            .catch(error => {
+                console.log('SW failed', error)
+            })
+    }
+})
---
+++ sw.js
@@ -0,0 +1,38 @@
+const staticCacheName = 'site-static-v5'
+const assets = [
+    '.',
+    'index.html',
+    'app.js',
+    'icons/icon_192.png',
+    'icons/apple-touch-icon_180.png',
+    'icons/apple-touch-icon_120.png',
+    'style.css'
+]
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('Кэширование ресурсов')
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener('active', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    )
+})
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)
+        })
+    )
+})
